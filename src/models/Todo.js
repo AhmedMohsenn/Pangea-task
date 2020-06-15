3c752b97166d8a26b37a5a586a54a47fbfd86d36
Ahmed Mohsen <ahmedmohsenn49@gmail.com>
2020-06-15 03:19:47 +0200

    [ADD] documentation with JSDoc

--- src/models/Todo.js
+++ src/models/Todo.js
@@ -1,17 +1,38 @@
+
+
+/**
+ * Class representing a Todo element.
+ */
 export class Todo {
+    /**
+     * Create a todo.
+     * @param {String} todoText - The text value.
+     */
     constructor(todoText) {
         this._text = todoText;
         this._completed = false;
     }
 
+    /**
+     * Get the text value.
+     * @return {String} the text value.
+     */
     get text() {
         return this._text;
     }
 
+    /**
+     * Get the completed value.
+     * @return {Boolean} the completed value.
+     */
     get completed() {
         return this._completed;
     }
 
+    /**
+     * Set the completed value.
+     * @param {Boolean} completed - the new value of completed.
+     */
     set completed(completed) {
         this._completed = completed;
     }
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,4 +1,5 @@
 import { Todo }  from "@/models/Todo.js";
+import { Todo } from "../../models/Todo";
 
 export default {
     namespaced: true,
@@ -8,35 +9,70 @@ export default {
     },
 
     getters: {
+        /**
+         * Get the todos.
+         * @param {Object} state - the current state of todo module.
+         */
         todos: state => state.todos,
     },
 
     mutations: {
+        /**
+         * Add new todo.
+         * @param {Object} state - the current state of todo module.
+         * @param {Todo} todo - the object of the todo.
+         */
         CREATE_TODO: (state, todo) => {
             state.todos.push(todo);
         },
 
+        /**
+         * Set the completed status of todo to true.
+         * @param {Object} state - the current state of todo module.
+         * @param {Number} todoIndex - the index of the todo in todos array.
+         */
         COMPLETE_TODO: (state, todoIndex) => {
             const currentTodo = state.todos[todoIndex];
             currentTodo.completed = true;
             state.todos[todoIndex] = currentTodo;
         },
 
+        /**
+         * Delete the todo with the give index.
+         * @param {Object} state - the current state of todo module.
+         * @param {Number} todoIndex - the index of the todo in todos array.
+         */
         DELETE_TODO: (state, todoIndex) => {
             state.todos.splice(todoIndex, 1);
         }
     },
 
     actions: {
+        /**
+         * Call the CREATE_TODO mutation to add new todo.
+         * @param {Function} commit - the function that calls the CREATE_TODO mutation.
+         * @param {String} todoText
+         */
         addTodo: ({ commit }, todoText) => {
             const todo = new Todo(todoText);
             commit('CREATE_TODO', todo);
         },
 
+
+        /**
+         * Call the COMPLETE_TODO mutation to add new todo.
+         * @param {Function} commit - the function that calls the COMPLETE_TODO mutation.
+         * @param {Number} todoIndex - the index of the todo in todos array.
+         */
         completeTodo: ({ commit }, todoIndex) => {
             commit('COMPLETE_TODO', todoIndex);
         },
 
+        /**
+         * Call the DELETE_TODO mutation to add new todo.
+         * @param {Function} commit - the function that calls the DELETE_TODO mutation.
+         * @param {Number} todoIndex - the index of the todo in todos array.
+         */
         deleteTodo: ({ commit }, todoIndex) => {
             commit('DELETE_TODO', todoIndex);
         }
